d83da7ad9bdb578099da7d8b206a1c105e702140
Seth Dixon <5654920324@r9tigermail.org>
2017-01-03 10:56:07 -0600

    Added customer object

---
+++ Centipac/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centipac
+{
+    class Customer
+    {
+        public string Registrant { get; set; }
+        public int adults { get; set; }
+        public int children { get; set; }
+        public string date { get; set; }
+
+    }
+}
